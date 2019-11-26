4b1a409fd6c62d3ac436b53447f8af57c97bba04
MicrochipQ <microchipq@gmail.com>
2019-11-27 01:07:21 +0200

    ✏️💡 Updating a module. "app"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { PasswordModule } from './password/password.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, PasswordModule],
   providers: [],
   bootstrap: [AppComponent],
 })
